411a44c11efd3ac03eb8a1e64ff99322b0c2dad9
Hafida <hafidabelaidagnaoui@gmail.com>
2023-11-28 18:27:10 +0000

    modification

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# - Configure SSH client for key-based authentication and disable password
+# authentication
+
+
+# Ensure SSH client uses the specified private key
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
