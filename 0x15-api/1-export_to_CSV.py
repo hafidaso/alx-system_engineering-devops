0d8097365d8dd5990e09c322b4700330f2853451
Hafida Belayd <hafidabelaidagnaoui@gmail.com>
2024-01-03 07:41:47 -0800

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,23 +3,21 @@
 import csv
 import requests
 import sys
-
-# Disable warnings (not recommended for production code)
 import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-
-    # Fetch user and todo data
-    user = requests.get(f"{url}users/{user_id}", verify=False).json()
+    user = requests.get(url + "users/{}".format(user_id), verify=False).json()
     username = user.get("username")
-    todos = requests.get(f"{url}todos", params={"userId": user_id}, verify=False).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}, verify=False).json()
 
-    # Write data to CSV
-    with open(f"{user_id}.csv", "w", newline="") as csvfile:
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for t in todos:
-            writer.writerow([user_id, username, t.get("completed"), t.get("title")])
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
 
+        
