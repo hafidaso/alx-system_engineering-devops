fa1413f5d676833c97105072305bdb3447ed8f2b
Hafida Belayd <hafidabelaidagnaoui@gmail.com>
2024-01-03 07:45:58 -0800

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,39 +1,24 @@
 #!/usr/bin/python3
-"""a module that request data and save in csv format"""
+# csv exported
 import csv
-import json
-import requests
+from requests import get
 from sys import argv
 
 
-if __name__ == "__main__":
-    if argv[1] is None or type(int(argv[1])) is not int:
-        exit()
-    filename = argv[1] + ".csv"
-    url = "https://jsonplaceholder.typicode.com"
-    try:
-        res_user = requests.get(url + "/users/" + argv[1])
-        user = res_user.json()
-    except:
-        exit()
-    try:
-        res_todo = requests.get(url + "/todos")
-        things = res_todo.json()
-    except:
-        exit()
-    EMPLOYEE_NAME = user.get("username", None)
+def cvsWrite(user):
+    """writes to csv"""
+    data = get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        user)).json()
+    name = get('https://jsonplaceholder.typicode.com/users/{}'.format(
+        user)).json().get('username')
+    employ_data = open('{}.csv'.format(user), 'w')
+    cwrite = csv.writer(employ_data, quoting=csv.QUOTE_ALL)
+    for line in data:
+        lined = [line.get('userId'), name,
+                 line.get('completed'), line.get('title')]
+        cwrite.writerow(lined)
+    employ_data.close()
 
-    with open(filename, "w") as f:
-        csv_fields = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-                      "TASK_TITLE"]
-        writer = csv.DictWriter(f, fieldnames=csv_fields)
-        writer.writeheader()
-        for thing in things:
-            row_dict = {}
-            if thing.get("userId", None) == int(argv[1]):
-                row_dict["USER_ID"] = thing.get("userId", None)
-                row_dict["USERNAME"] = EMPLOYEE_NAME
-                row_dict["TASK_COMPLETED_STATUS"] = thing.get("completed")
-                row_dict["TASK_TITLE"] = thing.get("title", None)
-                json.dumps(row_dict)
-                writer.writerow(row_dict) 
+
+if __name__ == "__main__":
+    cvsWrite(argv[1]) 
