f10bc7339ed3fd3b1c8e57eab9198714c1dc2dcb
Hafida Belayd <hafidabelaidagnaoui@gmail.com>
2024-01-03 07:48:49 -0800

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,23 +1,46 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
-import csv
+""" Using what you did in the task #0, extend your Python script
+to export data in the JSON format. """
+import json
 import requests
-import sys
-import urllib3
+from sys import argv
 
-# Disable SSL certificate verification
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id), verify=False).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}, verify=False).json()
+def Rest_API():
+    """ Get data """
+    # Validates if argument is integer and has index
+    try:
+        ID = int(argv[1])
+    except ValueError:
+        print("Value Error")
+        exit()
+    except IndexError:
+        print("Index Error")
+        exit()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [
-            writer.writerow([user_id, username, t.get("completed"), t.get("title")])
-            for t in todos
-        ]
+    # Get Method
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                        .format(ID))
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(ID))
+
+    # Variables
+    username = user.json().get('username')
+    datafile = {}
+    datafile[ID] = []
+
+    # export data in JSON format
+    with open("{}.json".format(ID), "w") as fo:
+        for data in todo.json():
+            text = data.get("title")
+            task = data.get("completed")
+            datafile[ID].append({
+                "task": text,
+                "completed": task,
+                "username": username,
+            })
+        json.dump(datafile, fo)
+
+
+if __name__ == '__main__':
+    Rest_API()
