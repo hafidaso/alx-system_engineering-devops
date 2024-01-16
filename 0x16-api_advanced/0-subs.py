f9808315a374f66a4fefb5200e209c52700d1ac0
Hafida Belayd <hafidabelaidagnaoui@gmail.com>
2024-01-16 08:03:22 -0800

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     Get the number of subscribers for a given subreddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'user-agent': 'request'}
+    headers = {'User-Agent': 'MyRedditBot/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
